a226cee4df844f1d375047cc6ae93536027afebf
yeeeee1015 <eric.ye04@yahoo.com>
2023-01-25 15:42:16 -0800

    Fixed reverse, reverseinplace

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,17 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReversedWithInts() {
+    int[] input1 = { 1, 2, 3};
+    assertArrayEquals(new int[]{3, 2, 1 }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReversedInPlaceWithInts() {
+    int[] input1 = { 1, 2, 3};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{3, 2, 1 }, input1);
+  }
 }
